e1583796bc9499aef033beac98cbe666469fa992
Torenjk <33992305+Torenjk@users.noreply.github.com>
2021-04-22 17:38:48 +0200

    read safe added

--- eeprom.c
+++ eeprom.c
@@ -15,3 +15,30 @@ uint8_t EE_READ_UNSEC(uint8_t adress_low, uint8_t adress_high){//Should only be 
     return **EE_DATA;
 }
 
+uint8_t EE_READ_SAFE(uint8_t adress_low, uint8_t adress_high){
+    if(adress_low == NULL) return NULL; //invalid
+    if(adress_high == NULL) return NULL; //invalid
+    if(0xFF < adress_low) return NULL; //Adress low out of Range
+    if(0x4 < adress_high) return NULL; //Adress high out of Range
+    *EE_CONTROL_REG = 0; //Reset EEPE Bit
+    *EE_ADRESS_LOW = adress_low;
+    *EE_ADRESS_HIGH = adress_high;
+    *EE_CONTROL_REG |= (1 << 0); //Trigger read
+    if(*EE_DATA == NULL)
+        {
+            chk_integrity();
+            return NULL; //invalid result
+        } 
+    return *EE_DATA;
+}
+
+bool chk_integrity(){
+    //TODO
+}
+
+int main(){
+
+    return 0;
+}
+
+
